f976eddddda6c5c89bcf0bc4d8737b1f26883986
RevolPierrot <70239446+RevolPierrot@users.noreply.github.com>
2020-11-25 11:31:21 +0100

    :construction: setting up api request for weather

--- src/components/Wetter.js
+++ src/components/Wetter.js
@@ -1,13 +1,28 @@
 import {React, Component} from 'react';
-import Default from "./Default";
+
+const API_URL = 'http://api.openweathermap.org/data/2.5/weather';
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
 class Wetter extends Component {
+
+    state = {
+        city: null,
+        weather: null,
+        error: null,
+    }
+
+    onButtonClick = () => {
+        const city = document.getElementById('city').value
+        //state.city setzen
+        console.info(this.setState({city: city, weather: null, error: null}))
+    }
 
     render() {
         return (
             <div className="container">
-                <h3>Meine Wetter Page</h3>
-                <Default message="Wetter Kram..."/>
+                <input id="city" placeholder="Please enter City"/>
+                <button onClick={this.onButtonClick}>Get Weather</button>
+                <h3>Weather for {this.state.city}</h3>
             </div>
         );
     }
